710ecf8fa18617afca5f6988abb66c1f8425c1e0
Some Subhra Patra <somesubhrapatra@gmail.com>
2018-08-21 01:37:16 +0530

    index page slider logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Jalpaiguri Birds';
--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -9,11 +9,55 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
   title = 'Home | Jalpaiguri Birds';
+  public sightingSlider: any;
+  public tourSlider: any;
+  public publicationSlider: any;
   constructor() {
 
   }
   ngOnInit() {
 
+    this.sightingSlider = [
+      {
+        'carouselItems': [{
+          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Beautiful Nuthatch.jpg',
+          'caption': 'Beautiful Nuthatch',
+          'user': 'some subhra',
+          'location': 'chapramari'
+        },
+        {
+          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/cutia.jpg',
+          'caption': 'Himalayan Cutia',
+          'user': 'babia',
+          'location': 'gajoldoba'
+        },
+        {
+          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Pitta_net.jpg',
+          'caption': 'Hooded Pitta',
+          'user': '',
+          'location': ''
+        }
+        ]
+      }, {
+        'carouselItems': [{
+          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/ruddy.JPG',
+          'caption': 'Ruddy Kingfisher',
+          'user': '',
+          'location': ''
+        },
+        {
+          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Sultan tit_sahay.jpg',
+          'caption': 'Sultan Tit',
+          'user': '',
+          'location': ''
+        },
+        {
+          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/yuhina.jpg',
+          'caption': 'Striped Yuhina',
+          'user': '',
+          'location': ''
+        }]
+      }]
   }
 
 }
