db17525c9f78523c7bfa424e509e680c5af867cb
Some Subhra Patra <somesubhrapatra@gmail.com>
2018-09-13 22:40:23 +0530

    two carousel with proper styling with latest article data import

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -9,9 +9,11 @@ import * as $ from 'jquery';
 })
 export class HomeComponent implements OnInit {
   title = 'Home | Jalpaiguri Birds';
-  public sightingSlider: any;
-  public tourSlider: any;
-  public publicationSlider: any;
+  public sightingSlider: Array<any> = [];
+  public smallScreenSightingSlider: Array<any> = [];
+  public tourSlider: Array<any> = [];
+  public latestArticles: Array<any> = [];
+
   constructor() {
 
   }
@@ -19,45 +21,104 @@ export class HomeComponent implements OnInit {
 
     this.sightingSlider = [
       {
-        'carouselItems': [{
-          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Beautiful Nuthatch.jpg',
-          'caption': 'Beautiful Nuthatch',
-          'user': 'some subhra',
-          'location': 'chapramari'
-        },
-        {
-          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/cutia.jpg',
-          'caption': 'Himalayan Cutia',
-          'user': 'babia',
-          'location': 'gajoldoba'
-        },
-        {
-          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Pitta_net.jpg',
-          'caption': 'Hooded Pitta',
-          'user': '',
-          'location': ''
-        }
+        'carouselItems': [
+          {
+            'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Beautiful Nuthatch.jpg',
+            'caption': 'Beautiful Nuthatch',
+            'user': 'some subhra',
+            'location': 'chapramari'
+          },
+          {
+            'src': '../../../assets/images/Other-Images/Home-Page Slider Images/cutia.jpg',
+            'caption': 'Himalayan Cutia',
+            'user': 'babia',
+            'location': 'gajoldoba'
+          },
+          {
+            'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Pitta_net.jpg',
+            'caption': 'Hooded Pitta',
+            'user': '',
+            'location': ''
+          }
         ]
       }, {
-        'carouselItems': [{
-          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/ruddy.JPG',
-          'caption': 'Ruddy Kingfisher',
-          'user': '',
-          'location': ''
-        },
-        {
-          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Sultan tit_sahay.jpg',
-          'caption': 'Sultan Tit',
-          'user': '',
-          'location': ''
-        },
-        {
-          'src': '../../../assets/images/Other-Images/Home-Page Slider Images/yuhina.jpg',
-          'caption': 'Striped Yuhina',
-          'user': '',
-          'location': ''
-        }]
-      }]
+        'carouselItems': [
+          {
+            'src': '../../../assets/images/Other-Images/Home-Page Slider Images/ruddy.JPG',
+            'caption': 'Ruddy Kingfisher',
+            'user': '',
+            'location': ''
+          },
+          {
+            'src': '../../../assets/images/Other-Images/Home-Page Slider Images/Sultan tit_sahay.jpg',
+            'caption': 'Sultan Tit',
+            'user': '',
+            'location': ''
+          },
+          {
+            'src': '../../../assets/images/Other-Images/Home-Page Slider Images/yuhina.jpg',
+            'caption': 'Striped Yuhina',
+            'user': '',
+            'location': ''
+          }]
+      }];
+
+    this.sightingSlider.forEach((items: any, index: any) => {
+      items.carouselItems.forEach((item, index) => {
+        this.smallScreenSightingSlider.push(item);
+      });
+    });
+
+    this.tourSlider = [
+      {
+        'src': '../../../assets/images/Other-Images/Birding Tour-Images/buxa.jpg',
+        'location': 'Buxa Tiger Reserve'
+      },
+      {
+        'src': '../../../assets/images/Other-Images/Birding Tour-Images/gajoldoba.JPG',
+        'location': 'Gajoldoba'
+      },
+      {
+        'src': '../../../assets/images/Other-Images/Birding Tour-Images/buxa-hills.JPG',
+        'location': 'Buxa Hills'
+      },
+      {
+        'src': '../../../assets/images/Other-Images/Birding Tour-Images/lava.jpg',
+        'location': 'Lava'
+      }
+    ];
+
+    this.latestArticles = [
+      {
+        'location': 'Royal Manas NP',
+        'type': 'Trip Report',
+        'link': '#',
+        'src': '../../../assets/images/Other-Images/Trip-Reports-Images/Royal-Manas/Royal-Manas.jpg'
+      },
+      {
+        'location': 'North Sikkim',
+        'type': 'Birding Spot',
+        'link': '#',
+        'src': '../../../assets/images/Other-Images/Hot - Spots-Images/North-Sikkim/North-Sikkim-Main.jpg'
+      },
+      {
+        'location': 'Buxa TR',
+        'type': 'Birding Spot',
+        'link': '#',
+        'src': '../../../assets/images/Other-Images/Hot - Spots-Images/Buxa Tiger Reserve/Buxa Habitat.jpg'
+      },
+      {
+        'location': 'Zuluk',
+        'type': 'Birding Spot',
+        'link': '#',
+        'src': '../../../assets/images/Other-Images/Hot - Spots-Images/Zuluk-lungthu/zuluk.jpg'
+      }
+    ]
+
+
+
   }
+
+
 
 }
