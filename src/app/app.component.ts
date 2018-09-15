a2a749b84d90496e6dad0c9d8ae29271ce340b94
Some Subhra Patra <somesubhrapatra@gmail.com>
2018-09-15 15:06:19 +0530

    footer article slider complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,20 +17,30 @@ export class AppComponent {
   ngOnInit() {
     this.articleSlider = [
       {
-        'caption': 'The Amur Falcon Story',
-        'users':'',
-        'link': '#',
-        'src': '../assets/images/Other-Images/article-slider/amur.jpg'
-      },{
-        'caption': 'Nesting Green Cochoa in Buxa Tiger Reserve',
-        'users':'Biswapriya Rahut',
-        'link': '#',
+        'caption': 'Nesting of Green Cochoa in Buxa Tiger Reserve',
+        'users': 'Biswapriya Rahut',
+        'link': '../assets/article-docs/Nesting Green Cochoa.pdf',
         'src': '../assets/images/Other-Images/article-slider/cochoa.png'
-      },{
+      }, {
+        'caption': 'First record of Rufous-throated Wren-babbler from Buxa Tiger Reserve',
+        'users': 'Biswapriya Rahut',
+        'link': '../assets/article-docs/Rufous-throated_Wren-babbler.pdf',
+        'src': '../assets/images/Other-Images/article-slider/rt-wren-babbler-buxa.png'
+      }, {
+        'caption': 'Records of Ward’s Trogon in Neora Valley National Park',
+        'users': 'Biswapriya Rahut',
+        'link': '../assets/article-docs/Wards_Trogon.pdf',
+        'src': '../assets/images/Other-Images/article-slider/trogon.png'
+      }, {
         'caption': 'First photographic record of Blyth’s Rosefinch',
-        'users':'Biswapriya Rahut, Mousumi Dutta & Chewang Rinchen Bonpo',
-        'link': '#',
+        'users': 'Biswapriya Rahut, Mousumi Dutta & Chewang Rinchen Bonpo',
+        'link': '../assets/article-docs/Blyth_Rosefinch.pdf',
         'src': '../assets/images/Other-Images/article-slider/rosef.png'
+      }, {
+        'caption': 'The status of the Black-rumped Magpie in India',
+        'users': 'Robert P. Prŷs-Jones & Pamela C. Rasmussen',
+        'link': '../assets/article-docs/Black_Rumped_Magpie.pdf',
+        'src': '../assets/images/Other-Images/article-slider/magpie.png'
       }
     ]
   }
